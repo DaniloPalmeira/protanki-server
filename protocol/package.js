61bd593890fa736bcfec9d1d51cece6a840ce67b
DaniloPalmeira <srdanlino@icloud.com>
2023-05-10 05:40:44 -0300

    map addition

--- protocol/package.js
+++ protocol/package.js
@@ -66,9 +66,9 @@ const vectorPacket = (coord, optional = false) => {
 	}
 
 	packet.writeBoolean(false);
-	packet.writeFloat(coord.x ?? 0);
-	packet.writeFloat(coord.y ?? 0);
-	packet.writeFloat(coord.z ?? 0);
+	packet.writeFloat(coord?.x ?? 0);
+	packet.writeFloat(coord?.y ?? 0);
+	packet.writeFloat(coord?.z ?? 0);
 
 	return packet;
 };

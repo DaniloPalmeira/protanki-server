225288909174825537a18a719db360dc8cb0f85f
DaniloPalmeira <srdanlino@icloud.com>
2023-05-15 01:32:07 -0300

    Update ProTankiCommands.js

--- src/classes/ProTankiCommands.js
+++ src/classes/ProTankiCommands.js
@@ -279,10 +279,12 @@ module.exports = class {
 		let extraZ = 0;
 		if (argCount == 2) {
 			extraZ = parseInt(args[1]);
+			bonusPosition.z += extraZ;
+		} else {
+			bonusPosition.z += 1000;
 		}
 
 		const bonusPosition = JSON.parse(JSON.stringify(position));
-		bonusPosition.z += 1000 + extraZ;
 		const bonusPacket = new ByteArray(
 			createBonusPacket(bonusName, bonusPosition, timeMS)
 		);
